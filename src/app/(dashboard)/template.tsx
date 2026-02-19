a919e0e0851b955877fb17dfe9ba51615cea102b
FPH-DEV89 <fphil89@example.com>
2026-02-19 19:34:28 +0100

    fix: temporary remove PageTransition to solve blank page

--- src/app/(dashboard)/template.tsx
+++ src/app/(dashboard)/template.tsx
@@ -1,5 +1,3 @@
-import { PageTransition } from "@/components/shared/page-transition"
-
 export default function Template({ children }: { children: React.ReactNode }) {
-    return <PageTransition>{children}</PageTransition>
+    return <>{children}</>
 }
